0bf6b472e7a7a933269781667c1aa604d828de09
Faisal Sayyed <sayyedfaisal06@gmail.com>
2024-04-26 01:24:27 +0530

    basic assets update and installed shadcn

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body
+        className={cn("w-full min-h-screen flex flex-col", poppins.className)}
+      >
+        {children}
+      </body>
     </html>
   );
 }
